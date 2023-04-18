51052532aa30d94418d2a2c6b6de3f8accf6d4bf
Anatoli Nikolaevich <tolya.tar@gmail.com>
2023-04-18 19:02:57 +0300

    avatar in progress

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
 import {MenuButton} from "./menuButton/menuButton";
+import {HeaderTitle} from "./headerTitle/headerTitle";
+import {useResponsive} from "../hooks/useResponsive";
 
 import './styles.scss';
-import {HeaderTitle} from "./headerTitle/headerTitle";
+import {UserAvatar} from "./userAvatar/userAvatar";
 
 export const Header = () => {
+
+    const {isMobile, isTablet, isDesktop} = useResponsive()
+
     return (
         <div className="header">
-            <MenuButton />
-            <HeaderTitle />
+            {isMobile || isTablet ? <MenuButton/> : null}
+            <HeaderTitle/>
+            {isDesktop ? <UserAvatar/> : null}
         </div>
     );
 };
