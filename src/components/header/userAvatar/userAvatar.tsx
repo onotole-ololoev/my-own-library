5b39bddd682a35cf38b3591e4ecb310ab74d85e6
Anatoli Nikolaevich <tolya.tar@gmail.com>
2023-04-20 12:50:25 +0300

    avatar in progress

--- src/components/header/userAvatar/userAvatar.tsx
+++ src/components/header/userAvatar/userAvatar.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import avatarUser from '../../../assets/avatar.jpg'
+import avatarUser from '../../../assets/erofeev-moskva-petushki.jpg'
 
 import './styles.scss'
 
 export const UserAvatar = () => {
     return (
         <div className={'user-avatar'}>
-            <img src={avatarUser} alt="your avatar"/>
+            <img className={'user-avatar__img'} src={avatarUser} alt="your avatar"/>
         </div>
     );
 };
