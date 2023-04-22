7b60d1b9b3fbbee722bde1b2957ae0aa0bae4c2e
Anatoli Nikolaevich <tolya.tar@gmail.com>
2023-04-22 15:10:39 +0300

    form to add new book created

--- src/pages/adminPage/adminPage.tsx
+++ src/pages/adminPage/adminPage.tsx
@@ -1,4 +1,4 @@
-import { Button, Form, Input, Select } from 'antd';
+import { Button, Form, Input, Select, InputNumber } from 'antd';
 import React from 'react';
 
 const { Option } = Select;
@@ -15,7 +15,10 @@ const tailLayout = {
 export const AdminPage: React.FC = () => {
     const [form] = Form.useForm();
 
-    const onGenderChange = (value: string) => {
+    const onBindingChange = (value: string) => {
+        console.log(value)
+    };
+    const onGenreChange = (value: string) => {
         console.log(value)
     };
 
@@ -35,26 +38,75 @@ export const AdminPage: React.FC = () => {
             onFinish={onFinish}
             style={{ maxWidth: 600 }}
         >
-            <Form.Item name="Book Title" label="Book Title" >
+            <Form.Item name="book Title" label="book Title" >
+                <Input placeholder="book title"/>
+                {/*{ isMobile ? <span>title</span> : null}*/}
+            </Form.Item>
+            <Form.Item name="author" label="author" >
                 <Input />
             </Form.Item>
-            <Form.Item name="Genre" label="Genre">
+            <Form.Item label="year">
+                <InputNumber />
+            </Form.Item>
+            <Form.Item name="publishing house" label="publishing house" >
+                <Input />
+            </Form.Item>
+            <Form.Item label="pages">
+                <InputNumber />
+            </Form.Item>
+            <Form.Item name="binding" label="binding">
                 <Select
-                    placeholder="Select a book genre"
-                    onChange={onGenderChange}
+                    placeholder="Select a type of binding"
+                    onChange={onBindingChange}
                     allowClear
                 >
-                    <Option value="male">Genre</Option>
-                    <Option value="female">Genre</Option>
-                    <Option value="other">Genre</Option>
+                    <Option value="hard">hard</Option>
+                    <Option value="soft">soft</Option>
                 </Select>
+            </Form.Item>
+            <Form.Item name="format" label="format" >
+                <Input placeholder={'70x100'}/>
+            </Form.Item>
+            <Form.Item name="genre" label="genre">
+                <Select
+                    placeholder="Select a book genre"
+                    onChange={onGenreChange}
+                    allowClear
+                >
+                    <Option value="all">all</Option>
+                    <Option value="business">business</Option>
+                    <Option value="detective">detective</Option>
+                    <Option value="foreign">foreign</Option>
+                    <Option value="history">history</Option>
+                    <Option value="classic">classic</Option>
+                    <Option value="psycho">psycho</Option>
+                    <Option value="computer">computer</Option>
+                    <Option value="culture">culture</Option>
+                    <Option value="science">science</Option>
+                    <Option value="journalistic">journalistic</Option>
+                    <Option value="reference">reference</Option>
+                    <Option value="fantastic">fantastic</Option>
+                    <Option value="humor">humor</Option>
+                </Select>
+            </Form.Item>
+            <Form.Item name="weight" label="weight" >
+                <Input placeholder={'350 g.'}/>
+            </Form.Item>
+            <Form.Item name="isbn" label="isbn" >
+                <Input  placeholder={'978-5-4461-0923-4'}/>
+            </Form.Item>
+            <Form.Item name="manufacturer" label="manufacturer" >
+                <Input placeholder={'ООО «Питер Мейл». РФ'}/>
+            </Form.Item>
+            <Form.Item name="description" label="description" >
+                <Input />
             </Form.Item>
             <Form.Item {...tailLayout}>
                 <Button type="primary" htmlType="submit">
-                    Submit
+                    add book
                 </Button>
                 <Button htmlType="button" onClick={onReset}>
-                    Reset
+                    reset
                 </Button>
             </Form.Item>
         </Form>
